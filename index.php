4e136b993758a99425f75c9ba54bb29b78083764
JJNinja9 <jrichards0511@gmail.com>
2024-06-21 19:58:47 -0500

    test

--- index.php
+++ index.php
@@ -1,72 +1,24 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <title>  </title>
-  <link rel="stylesheet" href=""> 
-  <!-- ----------------------------------
-			BIS1523/BIS2523 Documentation
-   Name:
-   Netid:
-   Date:
-   
-   Variables used:
-      <variable name>  <description of data variable will hold>
-  
-   -------------------------------------  -->
-</head>
-<body>
-    <h1>Results</h1> <br><br>
-    <?php
-        $object = $_POST['object'];
-        $radius = $_POST['radius'];
-        $height = $_POST['height'];
-        $surface_area;
-        $data_okay = true;
-        $link_to_input = "HW_4_Richards.html";
-        
-        if (!is_numeric($radius))
+<?php
+    print "ATM test\n\n";
+    print "1. Withdrawal\n";
+    print "2. Deposit\n";
+    print "3. Other\n\n";
+
+    $balance = 500;
+
+        $option = readline("Command: ");
+        //readline_add_history($line);
+
+        if ($option) == "1"
         {
-            print "Radius must be a number. ";
-            print "Please " . "<a href=\"$link_to_input\">click here</a>" . " to try again.<br>";
-            $data_okay = false;
-        }
-        
-        if (!is_numeric($height))
-        {
-            print "Height must be a number. ";
-            print "Please " . "<a href=\"$link_to_input\">click here</a>" . " to try again.<br>";
-            $data_okay = false;
-        }
-        
-        if (empty($object))
-        {
-            print "Please select an object. ";
-            print "Please " . "<a href=\"$link_to_input\">click here</a>" . " to try again.<br>";
-            $data_okay = false;
-        }
-    
-        if($data_okay)
-        {  
-            if($object=="cone")
+            print "How much would you like to withdraw?";
+            $amount = readline("Command: ");
+            
+            if ($amount > $balance)
             {
-               $surface_area= pi() * $radius * $radius + (pi() * $radius * (sqrt($radius * $radius + $height * $height)));
+                print "error. insufficient funds. calling police.";
             }
-            
-            elseif($object=="cylinder")
-            {
-                $surface_area= (2 * pi() * $radius * $height) + (2 * pi() * $radius * $radius);
-            }
-            
-            else{
-                $surface_area= (2 * pi() * $radius * $radius * $height) + (2 * pi() * $radius * $height);        
-            }
-            
-            print "The surface area of a $object with radius $radius and height $height is ".number_format($surface_area, 2);
 
         }
-    
-    ?>
-    
-    
-</body>
-</html>
+
+?>
