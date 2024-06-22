1f1505b888169eb8da500e345b296da7a91fd312
JJNinja9 <jrichards0511@gmail.com>
2024-06-21 19:18:19 -0500

    update test

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+
+<body>
+	<h1>40% Calculator</h1>
+	
+	<form action="myproject1.php" method="POST">
+		<input type = "text" name = "num" size = "5">
+		<input type = "submit" name="submit" value="Calculate!">
+	
+	</form>
+
 <?php
 
-echo "Hello Jeremiah!";
+
+	$num = $_POST['num'];
+	
+	print "40% of $num is" . number_format($num * .4, 2);
+</body>
+
+</html>
